db4949bb3e99d8b8d15e736c3cc44fbe570c961a
Brian-Konr <b08705038@ntu.edu.tw>
2021-12-14 23:14:48 +0800

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<h1>Home Page</h1>} />
+          <Route exact path="signIn" element = {<SignIn/>} />
+          <Route exact path="signUp" element = {<SignUp/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
