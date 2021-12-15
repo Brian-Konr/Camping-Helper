85fe2370d9db60e25d5a45ac2cb669adb1ec8a8e
Brian-Konr <b08705038@ntu.edu.tw>
2021-12-16 01:14:04 +0800

    signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from "./containers/Login";
+import SignUp from './containers/SignUp';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element = {<h1>Hello</h1>} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </div>
---
+++ frontend/src/containers/SignUp.js
@@ -0,0 +1,193 @@
+import React, { useState } from 'react';
+import { Form, Input, Select, Checkbox, Button } from 'antd';
+import '../css/signup.css'
+
+const { Option } = Select;
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const RegistrationForm = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+
+  return (
+      <div className='wrapper'>
+        <h1>創建新帳號</h1>
+        <Form
+            {...formItemLayout}
+            form={form}
+            className='signup-form'
+            name="register"
+            onFinish={onFinish}
+            initialValues={{}}
+            scrollToFirstError
+            >
+            <Form.Item
+                name="name"
+                label="姓名"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your name',
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item label="BirthDate" style={{ marginBottom: 0 }}>
+                <Form.Item
+                name="year"
+                rules={[{ required: true }]}
+                style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
+                >
+                <Input placeholder="Input birth year" />
+                </Form.Item>
+                <Form.Item
+                name="month"
+                rules={[{ required: true }]}
+                style={{ display: 'inline-block', width: 'calc(50% - 8px)', margin: '0 8px' }}
+                >
+                <Input placeholder="Input birth month" />
+                </Form.Item>
+            </Form.Item>
+
+            <Form.Item
+                name="phone"
+                label="Phone Number"
+                rules={[{ required: true, message: 'Please input your phone number!' }]}
+            >
+                <Input style={{ width: '100%' }} />
+            </Form.Item>
+
+
+            <Form.Item
+                name="email"
+                label="E-mail"
+                rules={[
+                {
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                },
+                {
+                    required: true,
+                    message: 'Please input your E-mail!',
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="nickname"
+                label="使用者名稱"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your nickname!',
+                    whitespace: true,
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your password!',
+                },
+                ]}
+                hasFeedback
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+                name="confirm"
+                label="Confirm Password"
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                {
+                    required: true,
+                    message: 'Please confirm your password!',
+                },
+                ({ getFieldValue }) => ({
+                    validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                    }
+
+                    return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                    },
+                }),
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+
+            <Form.Item
+                name="agreement"
+                valuePropName="checked"
+                rules={[
+                {
+                    validator: (_, value) =>
+                    value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+                },
+                ]}
+                {...tailFormItemLayout}
+            >
+                <Checkbox>
+                I have read the <a href="">agreement</a>
+                </Checkbox>
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+                <Button type="primary" htmlType="submit">
+                Register
+                </Button>
+                <a style={{"margin-left": "15px"}} href="">已經有帳號了？</a>
+            </Form.Item>
+        </Form>
+      </div>
+  );
+};
+
+export default RegistrationForm;
