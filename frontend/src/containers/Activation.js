01da9399c37d68296076f912b707cd3691cb4bd9
Brian-Konr <b08705038@ntu.edu.tw>
2022-01-08 15:48:16 +0800

    activation page done

--- frontend/src/containers/Activation.js
+++ frontend/src/containers/Activation.js
@@ -1,27 +1,41 @@
 import React from "react";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { message } from "antd";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Button, message, Modal } from "antd";
 import axios from 'axios';
 import instance from '../instance';
 
 const Activation = () => {
+    const navigate = useNavigate();
+    const [isModalVisible, setIsModelVisible] = useState(false);
     const { uid, token } = useParams();
-
     const activate = async() => {
         try {
             let res = await instance.post('/auth/users/activation/', {
                 uid: uid,
                 token: token
             });
-            console.log(res.data);
+            if(res.response.status == 204) setIsModelVisible(true);
         } catch(error) {
-            if(error.response.status == 403) console.log("The user has been activated");
+            if(error.response.status == 403 || error.response.status == 400) console.log("activation failed!")
         }
     }
     activate();
     return(
         <div>
+            <Modal 
+                closable={false} 
+                title="帳號驗證確認" 
+                visible={isModalVisible}
+                footer={[
+                    <Button onClick={() => {navigate('/login')}}>
+                        OK
+                    </Button>
+                ]}
+            >
+                <p>驗證成功!</p>
+                <p>系統將導至登入頁面...</p>
+            </Modal>
             <ul>
                 <li>{`uid: ${uid}`}</li>
                 <li>{`token: ${token}`}</li>
