f9693c3c10e1761a42179bb44c93b921bd90f857
Brian-Konr <b08705038@ntu.edu.tw>
2022-01-11 00:51:07 +0800

    commit

--- frontend/src/containers/CreateActivity.js
+++ frontend/src/containers/CreateActivity.js
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import Appbar from "../components/Appbar";
-import Intro from "../components/Intro";
 import CreateInput from "../components/CreatInput";
 const CreateActivity = () => {
+    
+    const [activityName, setActivityName] = useState("2022 台灣大學資管營");
+    const [date, setDate] = useState(['2022-01-01', '2022-01-3']);
+    const [info, setInfo] = useState("活動資訊");
+    const [place, setPlace] = useState("活動地點");
+    const [fee, setFee] = useState(2500);
+    const [quota, setQuota] = useState(70);
+    const [precaution, setPrecaution] = useState("注意事項");
 
     return (
         <div>
@@ -11,8 +18,34 @@ const CreateActivity = () => {
                 {/* <div className="headerPic">
                     <img src='https://images.unsplash.com/photo-1634913940785-c17c505a9c1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'></img>
                 </div> */}
-                <Intro />
-                <CreateInput />
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <h1>營隊名稱: {activityName}</h1>
+                    <h2>From {date[0]} to {date[1]}</h2>
+                    <p>活動資訊:</p>
+                    {/*whiteSpace 可以讓 textarea 根據換行做輸出 */}
+                    <p style={{whiteSpace: 'pre-line'}}> {info} </p>
+                    <h2>活動地點: {place}</h2>
+                    <h2>活動費用: {fee}</h2>
+                    <h2>活動名額: {quota}</h2>
+                    <p>注意事項:</p>
+                    <p style={{whiteSpace: 'pre-line'}}> {precaution} </p>
+                </div>
+                <CreateInput 
+                    activityName={activityName}  
+                    date={date}
+                    info={info}
+                    place={place}
+                    fee={fee}
+                    quota={quota}
+                    precaution={precaution}
+                    setActivityName={setActivityName}
+                    setDate={setDate}
+                    setInfo={setInfo}
+                    setPlace={setPlace}
+                    setFee={setFee}
+                    setQuota={setQuota}
+                    setPrecaution={setPrecaution}
+                />
             </div>
         </div>
     )
