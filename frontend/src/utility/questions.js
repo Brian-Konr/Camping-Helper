b1f8b6caf507a7158c3b4ccec92330b871141e49
Brian-Konr <b08705038@ntu.edu.tw>
2022-01-12 16:08:05 +0800

    put questions array into utility for different usage

---
+++ frontend/src/utility/questions.js
@@ -0,0 +1,25 @@
+export const OPTIONS = [
+    {title: '姓名', key: 1},
+    {title: '性別 (生理男 / 生理女)', key: 2},
+    {title: '國籍', key: 3},
+    {title: '身分證字號', key: 4},
+    {title: '出生年月日', key: 5},
+    {title: '學校名稱與年級', key: 6},
+    {title: '特殊疾病', key: 7},
+    {title: 'Facebook 連結', key: 8},
+    {title: '用餐習慣 (葷 / 素)', key: 9},
+    {title: '電子信箱', key: 10},
+    {title: '聯絡電話', key: 11},
+    {title: '監護人姓名', key: 12},
+    {title: '與監護人之關係', key: 13},
+    {title: '監護人之聯絡電話', key: 14},
+    {title: '自我介紹', key: 15},
+    {title: '特殊經歷', key: 16},
+    {title: '報名動機', key: 17},
+    {title: '自我期許', key: 18},
+    {title: '其他意見', key: 19}
+]
+
+export default {
+    OPTIONS,
+}
